2b7cc52e42cfcde1c88025d8b17723d40ad589bc
Jeff Bencin <jeff.bencin@gmail.com>
2017-02-23 16:34:24 -0500

    Delete .kde4

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ln -si $(pwd)/bin             ~/bin
-ln -si $(pwd)/.gitconfig      ~/.gitconfig
-ln -si $(pwd)/.vimrc          ~/.vimrc
-ln -si $(pwd)/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
-
-echo ""                    >> ~/.bashrc
-echo ". ~/bin/bashrc.bash" >> ~/.bashrc
