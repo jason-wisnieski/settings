00c1a519357c8c1690e96b3db68caae2ceed89c4
Jeff Bencin <jeff.bencin@gmail.com>
2017-02-23 12:27:04 -0500

    Add config for fish shell

---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,26 @@
+# Fish git prompt
+set __fish_git_prompt_show_informative_status true
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_prefix normal
+set __fish_git_prompt_color_suffix normal
+set fish_color_cwd -o brgreen
+
+function fish_prompt --description 'Write out the prompt'
+    set -l color_cwd
+    set -l suffix
+    switch $USER
+        case root toor
+            if set -q fish_color_cwd_root
+                set color_cwd $fish_color_cwd_root
+            else
+                set color_cwd $fish_color_cwd
+            end
+            set suffix '#'
+        case '*'
+            set color_cwd $fish_color_cwd
+            set suffix '$'
+    end
+
+    echo -n -s "$USER" @ (prompt_hostname) ':' (set_color $color_cwd) (prompt_pwd) (set_color normal) (__fish_git_prompt '(%s)') (set_color normal) "$suffix "
+end
+
--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -si $(pwd)/bin             ~/bin
 ln -si $(pwd)/.gitconfig      ~/.gitconfig
 ln -si $(pwd)/.vimrc          ~/.vimrc
+ln -si $(pwd)/.config/fish/functions/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
 
-echo ""                    >> ~/.bashrc 
-echo ". ~/bin/bashrc.bash" >> ~/.bashrc 
+echo ""                    >> ~/.bashrc
+echo ". ~/bin/bashrc.bash" >> ~/.bashrc
